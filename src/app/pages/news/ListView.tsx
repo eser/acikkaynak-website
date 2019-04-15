de5978e504176409d5b5a31b07e337862f1cf8eb
Okan DAVUT <davut.okan@gmail.com>
2019-04-16 01:12:49 +0300

    News page development

---
+++ src/app/pages/news/ListView.tsx
@@ -0,0 +1,100 @@
+import React, { useState, useEffect } from 'react';
+import NewDetailView from './NewDetailView';
+import * as bulmaStyles from 'bulma';
+import layoutStyles from '../../layouts/default/assets/styles.scss'
+import { stat } from 'fs';
+
+function ListView(props) {
+
+    const [isDetailOpen, setDetail] = useState(false);
+    const [clickedNew, setclickedNew] = useState(props.news[0]);
+    const [fromAllNews, getFromAllNews] = useState(false);
+    const [seeAllNews, setAllNews] = useState(false);
+    const [data, setData] = useState(props.news);
+    const [filterState, setFilterState] = useState(false);
+
+    var tags = [];
+    var newsList = [];
+    if (props.news.length > 0) newsList = props.news;
+    props.news.map((item, i) => {
+        item.tags.map((tag, i) => {
+            if (!tags.includes(tag)) tags.push(tag)
+        })
+    }); 
+    function showAllNews(i) {
+        setDetail(false)
+        setclickedNew(props.news[i])
+        setAllNews(true)
+    }
+    function openDetailPage(item, allNewsShow) {
+        setDetail(true)
+        setclickedNew(item)
+        if (allNewsShow)
+            setAllNews(true)
+        else
+            setAllNews(false)
+    }
+    function listTagNews(tagItem) {
+        newsList = props.news;
+        newsList = props.news.filter(item => { if (item.tags.includes(tagItem)) return item; });
+        setData(newsList)
+        setFilterState(true)
+    }
+    useEffect(
+        () => {
+        },
+        [data]
+    );
+    if (isDetailOpen) {
+        return (
+            <>
+                <div>
+                    <NewDetailView content={clickedNew} />
+                </div>
+            </>
+        )
+    }
+    else if (fromAllNews || seeAllNews) {
+        return (
+            <>
+                <h1 className={bulmaStyles.title}>Tüm Haberler</h1>
+                Etiketler :  {tags.map((tagItem, k) =>
+                    <a onClick={() => listTagNews(tagItem)} key={k}>{tagItem}&nbsp;&nbsp;&nbsp;&nbsp;</a>
+                )}
+                {filterState == true ? <a onClick={() => {setData(props.news);setFilterState(false)}}>Filtre Kaldır</a> :''}
+                <ul>
+                    {data.map((item, i) =>
+                        <li key={i} ><a key={i} onClick={() => openDetailPage(item, true)}>{item.title}</a>
+                        </li>
+                    )}
+                </ul>
+            </>
+        )
+    }
+    else {
+        return (
+            <>
+                <h1 className={bulmaStyles.title}>Haberler</h1>
+                <ul>
+                    {data.map((item, i) => {
+                        if (i < 2)
+                            return (
+                                <li key={i} ><a key={i} onClick={() => openDetailPage(item, false)}>{item.title}</a>
+                                </li>
+                            );
+                        if (i >= 2)
+                            return (
+                                <button key={"showAllNews"} onClick={() => showAllNews(i)}>
+                                    Tüm haberleri gör
+                                 </button>
+                            )
+                    }
+                    )}
+                </ul>
+            </>
+        )
+    }
+}
+export {
+    ListView as default,
+};
---
+++ src/app/pages/news/news.tsx
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react';
+import ListView from './ListView';
+import * as bulmaStyles from 'bulma';
+
+function News() {
+    const [state, setState] = useState({
+        news: [{
+            id: 1,
+            title: "THY: O ülkeye karşılıklı seferler iptal edildi!",
+            content:"Türk Hava Yolları'ndan yapılan açıklamada şu ifadeler kullanıldı: Sudan'da yaşanan askeri darbenin ardından hava sahasının kapatılması sonucu başkent Hartum'a planlanan karşılıklı seferler iptal edildi",
+            tags : ["THY","Turk Hava Yolları"]
+        },
+        {
+            id: 2,
+            title: "7 başkan, 9 teknik adam, onlarca yıldız! Hiçbiri başaramadı",
+            content:"Spor Toto Süper Lig'in 28. haftasında 14 Nisan Pazar günü Fenerbahçe'ye konuk olacak Galatasaray'da son 7 kulüp başkanı, Kadıköy'de galibiyet sevinci yaşamadı.",
+            tags : ["GS","FB"]
+        },
+        {
+            id: 3,
+            title: "test",
+            content:"Spor Toto Süper Lig'in 28. haftasında 14 Nisan Pazar günü Fenerbahçe'ye konuk olacak Galatasaray'da son 7 kulüp başkanı, Kadıköy'de galibiyet sevinci yaşamadı.",
+            tags : ["GS","FB"]
+        }]
+    });
+        return (
+            <>
+                <ListView news={state.news}/>    
+            </>
+          )
+    };
+    export {
+    News as default,
+};
