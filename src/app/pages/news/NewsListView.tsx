2109183ea2c6a6a107ee06f98c6266c0e7d5e3c0
Okan DAVUT <davut.okan@gmail.com>
2019-04-22 20:27:46 +0300

    News page architecture was changed.

--- src/app/pages/news/NewsListView.tsx
+++ src/app/pages/news/NewsListView.tsx
@@ -1,96 +1,141 @@
-import React, { useState, useEffect } from 'react';
-import NewDetailView from './NewDetailView';
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
 import * as bulmaStyles from 'bulma';
 import customNewsStyle from './style.scss';
 
-function ListView(props) {
-
-    const [isDetailOpen, setDetail] = useState(false);
-    const [clickedNew, setclickedNew] = useState(props.news[0]);
-    const [data, setData] = useState(props.news);
-    const [tag, setCurrentTag] = useState(null);
+function NewsListView(props) {
     const [currentPage, setCurrentPage] = useState(1);
     const [newsPerPage, setNewsPerPage] = useState(2);
 
-    const indexOfLastTodo = currentPage * newsPerPage;
-    const indexOfFirstTodo = indexOfLastTodo - newsPerPage;
-    const currentTodos = data.slice(indexOfFirstTodo, indexOfLastTodo);
-    const pageNumbers = [];
-    const newsList = props.news;
-    const tags = newsList.reduce(
+    const tags = props.news.reduce(
         (acc, cur) => [...acc, ...cur.tags.filter(x => !acc.includes(x))],
         []
     );
-    function openDetailPage(item) {
-        setDetail(true)
-        setclickedNew(item)
-    }
-    function listTagNews(tagItem) {
-        console.log(tagItem);
-        setData(newsList.filter(item => { if (item.tags.includes(tagItem)) return item; }));
-    }
-    function deleteFilter() {
-        setData(props.news);
+
+    let news = props.news;
+
+    if (props.tag !== undefined) {
+        news = news.filter(item => item.tags.includes(props.tag));
     }
 
-    useEffect(() => {
-
-    }, ["data"]);
-    const renderTodos = currentTodos.map((newItem, index) => {
-        return <li className={customNewsStyle.new} key={index} onClick={() => openDetailPage(newItem)}>{newItem.title}</li>;
-    });
-    for (let i = 1; i <= Math.ceil(data.length / newsPerPage); i++) {
-        pageNumbers.push(i);
-    }
-    let tagsArea = null;
-    if (tags.length > 0) {
-        tagsArea = tags.map((tagItem, number) => {
-            return (
-                <li
-                    className={customNewsStyle.tag}
-                    key={number}
-                    id={number}
-                    onClick={function () { setCurrentTag(number); listTagNews(tagItem) }}
-                >
-                    {tagItem}
-                </li>
-            );
-        });
-    }
-
-    const renderPageNumbers = pageNumbers.map(number => {
-        return (
-            <li
-                className={customNewsStyle.pagingItems}
-                key={number}
-                id={number}
-                onClick={() => setCurrentPage(number)}
-            >
-                {number}
-            </li>
-        );
-    });
-    if (isDetailOpen) {
-        return (
-
-            <NewDetailView content={clickedNew} />
-        )
-    }
     return (
-        <div>
+        <>
             <h5>{currentPage}. sayfa haberleri</h5>
+
+            <ul className={customNewsStyle.tagBox}>
+                {JSON.stringify(props.tag)}
+                {tags.map((tagItem, number) => {
+                    return (
+                        <NavLink key={number} to={`/news/tags/${encodeURIComponent(tagItem)}`}>
+                            <li
+                                className={customNewsStyle.tag}
+                            >
+                                {tagItem}
+                            </li>
+                        </NavLink>
+                    );
+                })}
+                <NavLink key="0" to={`/news/`}>
+                    <li
+                        className={customNewsStyle.tag}
+                    >
+                        X
+                    </li>
+                </NavLink>
+            </ul>
+
             <ul className={customNewsStyle.newsList}>
-                {renderTodos}
+                {news.map((newsItem, number) => {
+                    return (
+                        <NavLink key={number} to={`/news/detail/${encodeURIComponent(newsItem.slug)}`}>
+                            <li className={customNewsStyle.new}>{newsItem.title}</li>
+                        </NavLink>
+                    );
+                })}
             </ul>
-            <ul className={customNewsStyle.tagBox}>{tagsArea}</ul>
-            <p className={customNewsStyle.tag}
-                onClick={() => deleteFilter()}> X </p>
+            
             <ul id="page-numbers" className={customNewsStyle.paging}>
-                {renderPageNumbers}
             </ul>
-        </div>
+        </>
     );
+
+    // function listTagNews(tagItem) {
+    //     setData(data.filter(item => { if (item.tags.includes(tagItem)) return item; }));
+    // }
+
+    // function deleteFilter() {
+    //     setData(props.news);
+    // }
+
+    // const indexOfLastTodo = currentPage * newsPerPage;
+    // const indexOfFirstTodo = indexOfLastTodo - newsPerPage;
+    // const currentTodos = newsList.slice(indexOfFirstTodo, indexOfLastTodo);
+    // const pageNumbers = [];
+    // let tagsArea = null;
+
+
+    // const tags = newsList.reduce(
+    //     (acc, cur) => [...acc, ...cur.tags.filter(x => !acc.includes(x))],
+    //     []
+    // );
+    
+    // const renderTodos = currentTodos.map((newItem, index) => {
+    //     return <li className={customNewsStyle.new} key={index} onClick={() => openDetailPage(newItem)}>{newItem.title}</li>;
+    // });
+    // for (let i = 1; i <= Math.ceil(data.length / newsPerPage); i++) {
+    //     pageNumbers.push(i);
+    // }
+
+    // if (tags.length > 0) {
+    //     tagsArea = tags.map((tagItem, number) => {
+    //         return (
+    //             <li
+    //                 className={customNewsStyle.tag}
+    //                 key={number}
+    //                 id={number}
+    //                 onClick={function () { listTagNews(tagItem) }}
+    //             >
+    //                 {tagItem}
+    //             </li>
+    //         );
+    //     });
+    // }
+
+    // const renderPageNumbers = pageNumbers.map(number => {
+    //     return (
+    //         <li
+    //             className={customNewsStyle.pagingItems}
+    //             key={number}
+    //             id={number}
+    //             onClick={() => setCurrentPage(number)}
+    //         >
+    //             {number}
+    //         </li>
+    //     );
+    // });
+
+    // if (isDetailOpen) {
+    //     return (
+    //         <NewDetailView content={clickedNew} />
+    //     )
+    // }
+    // return (
+    //     <div>
+    //         <h5>{currentPage}. sayfa haberleri</h5>
+    //         <ul className={customNewsStyle.tagBox}>{tagsArea}</ul>
+    //         <p className={customNewsStyle.tag}
+    //             onClick={() => deleteFilter()}> X </p>
+    //         <ul className={customNewsStyle.newsList}>
+    //             {renderTodos}
+    //         </ul>
+            
+    //         <ul id="page-numbers" className={customNewsStyle.paging}>
+    //             {renderPageNumbers}
+    //         </ul>
+    //     </div>
+    // );
 }
+
 export {
-    ListView as default,
+    NewsListView as default,
 };
--- src/app/pages/news/news.tsx
+++ src/app/pages/news/news.tsx
@@ -1,36 +1,51 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import NewsListView from './NewsListView';
+import NewsDetailView from './NewsDetailView';
 import * as bulmaStyles from 'bulma';
 
-function News() {
+function News(props) {
     const [news, setNews] = useState([
         {
             id: 1,
+            slug: "thy-o-ulkeye-karsilikli-sefer",
             title: "THY: O ülkeye karşılıklı seferler iptal edildi!",
             content: "Türk Hava Yolları'ndan yapılan açıklamada şu ifadeler kullanıldı: Sudan'da yaşanan askeri darbenin ardından hava sahasının kapatılması sonucu başkent Hartum'a planlanan karşılıklı seferler iptal edildi",
             tags: ["THY", "Turk Hava Yolları"]
         },
         {
             id: 2,
+            slug: "7-baskan-9-teknik-adam-onlarca",
             title: "7 başkan, 9 teknik adam, onlarca yıldız! Hiçbiri başaramadı",
             content: "Spor Toto Süper Lig'in 28. haftasında 14 Nisan Pazar günü Fenerbahçe'ye konuk olacak Galatasaray'da son 7 kulüp başkanı, Kadıköy'de galibiyet sevinci yaşamadı.",
-            tags: ["GS", "FB"]
+            tags: ["ts", "bjk"]
         },
         {
             id: 3,
+            slug: "test",
             title: "test",
             content: "Spor Toto Süper Lig'in 28. haftasında 14 Nisan Pazar günü Fenerbahçe'ye konuk olacak Galatasaray'da son 7 kulüp başkanı, Kadıköy'de galibiyet sevinci yaşamadı.",
             tags: ["GS", "FB"]
         },
         {
             id: 4,
+            slug: "haber",
             title: "haber",
             content: "Spor Toto Süper Lig'in 28. haftasında 14 Nisan Pazar günü Fenerbahçe'ye konuk olacak Galatasaray'da son 7 kulüp başkanı, Kadıköy'de galibiyet sevinci yaşamadı.",
             tags: ["GS", "FB"]
         }
     ]);
+
+    if (props.slug !== undefined) {
+        const currentNewsItem = news.find(x => x.slug === props.slug);
+
+        // as long as content exists...
+        if (currentNewsItem !== undefined) {
+            return <NewsDetailView content={currentNewsItem} />
+        }
+    }
+
     return (
-        <NewsListView news={news} />
+        <NewsListView news={news} {...props} />
     )
 };
 
