d5903e213aa8f106afe45b3faa80d25d9fb36cb5
Okan DAVUT <davut.okan@gmail.com>
2019-04-27 22:48:15 +0300

    Add news develoment

--- src/app/pages/addNews/AddNews.tsx
+++ src/app/pages/addNews/AddNews.tsx
@@ -5,15 +5,13 @@ import * as bulmaStyles from 'bulma';
 import customAddNewsStyle from './style.scss';
 
 function AddNews(props) {
-    const [news, setNews] = useState([
-        {
-            id: 1,
-            slug: "thy-o-ulkeye-karsilikli-sefer",
-            title: "THY: O ülkeye karşılıklı seferler iptal edildi!",
-            content: "Türk Hava Yolları'ndan yapılan açıklamada şu ifadeler kullanıldı: Sudan'da yaşanan askeri darbenin ardından hava sahasının kapatılması sonucu başkent Hartum'a planlanan karşılıklı seferler iptal edildi",
-            tags: ["THY", "Turk Hava Yolları"]
-        }
-    ]);
+    const [newsTitle, setNewsTitle] = useState("");
+    const [newsContent, setNewsContent] = useState("");
+    const [newsTags, setNewsTags] = useState("");
+    
+    function addNews() {
+        //Todo Add new development
+    }
 
     return (
         <>
@@ -23,19 +21,27 @@ function AddNews(props) {
                     <h3>Haber Ekle</h3>
                     <div className={customAddNewsStyle.field}>
                         <p>Haber Başlığı</p>
-                        <textarea className={customAddNewsStyle.newsTitle} cols={40} rows={3} ></textarea>
+                        <textarea onChange={(e) => setNewsTitle(e.target.value)} className={customAddNewsStyle.newsTitle} cols={40} rows={3} ></textarea>
                     </div>
                     <div className={customAddNewsStyle.field}>
                         <p>Haber İçeriği</p>
-                        <textarea className={customAddNewsStyle.newsContent} cols={40} rows={10} ></textarea>
+                        <textarea onChange={(e) => setNewsContent(e.target.value)} className={customAddNewsStyle.newsContent} cols={40} rows={10} ></textarea>
                     </div>
                     <div className={customAddNewsStyle.field}>
                         <p>Haber Etiketleri</p>
-                        <textarea className={customAddNewsStyle.tags} cols={40} rows={5} ></textarea>
+                        <textarea
+                            className={customAddNewsStyle.tags}
+                            cols={40}
+                            rows={5}
+                            onChange={(e) => setNewsTags(e.target.value)}
+                        ></textarea>
                     </div>
                 </div>
                 <div className={customAddNewsStyle.button}>
-                    <button> Ekle</button>
+                    <button onClick={addNews}> Ekle</button>
+                </div>
+                <div>
+                    {"'" + newsTitle + "','" + newsContent + "','" + newsTags + "'"}
                 </div>
             </div>
         </>
