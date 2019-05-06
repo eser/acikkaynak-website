bbb964581a667a7c444cd71d901347646b9dd1fd
okandavutcom <davut.okan@gmail.com>
2019-05-07 00:22:38 +0300

    code review fixes

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -3,11 +3,11 @@ import { Route, Switch } from 'react-router';
 
 import Layout from './layouts/default/layout';
 import Home from './pages/home/home';
-import News from './pages/news/News';
+import News from './pages/news/news';
 import About from './pages/about/about';
 import Events from './pages/events/Events';
 import NotFound from './pages/notFound/notFound';
-import AddNews from './pages/addNews/AddNews';
+import AddNews from './pages/addNews/addNews';
 
 function App() {
     return (
@@ -17,12 +17,12 @@ function App() {
 
 
             {/* news */}
+                        <Route path="/news/add/" exact={false} strict={true} render={() => <Layout><AddNews /></Layout>} />
             <Route path="/news/detail/:slug" exact={false} strict={true} render={(props) => <Layout><News {...props.match.params} /></Layout>} />
             <Route path="/news/tags/:tag/:pageNumber" exact={false} strict={true} render={(props) => <Layout><News {...props.match.params} /></Layout>} />
             <Route path="/news/tags/:tag/" exact={false} strict={true} render={(props) => <Layout><News {...props.match.params} /></Layout>} />
             <Route path="/news/:pageNumber" exact={false} strict={true} render={(props) => <Layout><News {...props.match.params} /></Layout>} />
             <Route path="/news/" exact={true} strict={true} render={() => <Layout><News /></Layout>} />
-            <Route path="/addNews/" exact={true} strict={true} render={() => <Layout><AddNews /></Layout>} />
 
 
             {/* events */}
