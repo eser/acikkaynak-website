b56c15d25ca4c3ef1a1abbe8b7c1ee789cdc2778
Okan DAVUT <davut.okan@gmail.com>
2019-07-25 00:22:14 +0300

    indentation and styling operations

--- src/app/pages/home/index.tsx
+++ src/app/pages/home/index.tsx
@@ -5,14 +5,14 @@ import * as bulmaStyles from 'bulma';
 function Home() {
     return (
         <>
-       Merhaba, <br/><br/>
+            Merhaba, <br /><br />
 
-Şu anda  altyapımızı yenilemekle meşguluz. <br/><br/>
+            Şu anda  altyapımızı yenilemekle meşguluz. <br /><br />
 
- Şimdilik Hakkımızda bölümünden bizimle ilgili bilgi alabilir veya Projeler bağlantısı altından açık kaynak proje indeksimize ulaşabilirsiniz. <br/><br/>
+            Şimdilik Hakkımızda bölümünden bizimle ilgili bilgi alabilir veya Projeler bağlantısı altından açık kaynak proje indeksimize ulaşabilirsiniz. <br /><br />
 
-Biz çalışırken açık kaynak dünyasından ve organizasyonumuzdan uzakta kalmamak için Twitter Hesabımızı takibe alabileceğiniz gibi,  <br/>
-Slack grubumuza katılmak için davetiye talebinde de bulunabilirsiniz.
+            Biz çalışırken açık kaynak dünyasından ve organizasyonumuzdan uzakta kalmamak için Twitter Hesabımızı takibe alabileceğiniz gibi,  <br />
+            Slack grubumuza katılmak için davetiye talebinde de bulunabilirsiniz.
         </>
     );
 }
