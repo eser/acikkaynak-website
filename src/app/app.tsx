eec8fbdf0dbeaa2f60d8f6d5f945f4572ce31971
Okan DAVUT <davut.okan@gmail.com>
2019-07-24 23:03:16 +0300

    Organizations page development done

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -9,7 +9,11 @@ import About from './pages/about/index';
 import Events from './pages/events/index';
 import EventsAdd from './pages/events/add';
 import EventsEdit from './pages/events/edit';
+import Organizations from './pages/organizations/index';
+import OrgsAdd from './pages/organizations/add';
+import OrgsEdit from './pages/organizations/edit';
 import NotFound from './pages/notFound/index';
+
 
 function App() {
     return (
@@ -27,11 +31,16 @@ function App() {
 
 
             {/* events */}
-            <Route path="/events/add/" exact={false} strict={true} render={() => <Layout><EventsAdd  {...props.match.params}/></Layout>} />
+            <Route path="/events/add/" exact={false} strict={true} render={() => <Layout><EventsAdd/></Layout>} />
             <Route path="/events/detail/:slug" exact={false} strict={true} render={(props) => <Layout><Events {...props.match.params} /></Layout>} />
             <Route path="/events/edit/:slug/:type" exact={false} strict={true} render={(props) => <Layout><Events {...props.match.params} /></Layout>} />
             <Route path="/events/" exact={true} strict={true} render={() => <Layout><Events /></Layout>} />
 
+            {/* orgs */}
+            <Route path="/orgs/add/" exact={false} strict={true} render={() => <Layout><OrgsAdd /></Layout>} />
+            <Route path="/orgs/detail/:slug" exact={false} strict={true} render={(props) => <Layout><Organizations {...props.match.params} /></Layout>} />
+            <Route path="/orgs/edit/:slug/:type" exact={false} strict={true} render={(props) => <Layout><Organizations {...props.match.params} /></Layout>} />
+            <Route path="/orgs/" exact={true} strict={true} render={() => <Layout><Organizations /></Layout>} />
 
             {/* about */}
             <Route path="/about/" exact={true} strict={true} render={() => <Layout><About /></Layout>} />
---
+++ src/app/pages/organizations/add.tsx
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+ import customAddOrgsStyle from './add.scss';
+import { NavLink } from 'react-router-dom';
+
+function OrgsAdd(props) {
+
+    const [orgsTitle, setorgsTitle] = useState('Organizasyon Başlığı');
+    const [orgsContent, setorgsContent] = useState('Organizasyon İçeriği');
+    const [orgsCategory, setorgsCategory] = useState('1');
+
+    function addOrgs() {
+        // TODO ADD EVENTS ACTION
+    }
+    function goBackButton(event) {
+        history.back();
+        event.preventDefault();
+    }
+    return (
+        <>
+            <div className={customAddOrgsStyle.container}>
+                <NavLink key="0" to={`/events/`} onClick={goBackButton}>
+                        Geri Dön
+                </NavLink>
+                <div className={customAddOrgsStyle.form}>
+                    <h3>Etkinlik Ekle</h3>
+                    <div className={customAddOrgsStyle.field}>
+                        <p>Etkinlik Başlığı</p>
+                        <textarea onChange={(e) => setorgsTitle(e.target.value)}
+                            className={customAddOrgsStyle.orgsTitle} cols={40} rows={3} ></textarea>
+                    </div>
+                    <div className={customAddOrgsStyle.field}>
+                        <p>Etkinlik İçeriği</p>
+                        <textarea onChange={(e) => setorgsContent(e.target.value)}
+                            className={customAddOrgsStyle.orgsContent} cols={40} rows={10} ></textarea>
+                    </div>
+                    <div className={customAddOrgsStyle.field}>
+                        <p>Etkinlik Kategorisi Etiketleri</p>
+                        <select type="combobox" onChange={(e) => setorgsCategory(e.target.value)}
+                            className={customAddOrgsStyle.category}>
+                            <option value="0">Software </option>
+                            <option value="1">Hardware</option>
+                        </select>
+                    </div>
+                </div>
+                <div className={customAddOrgsStyle.button}>
+                    <button onClick={addOrgs}> Ekle</button>
+                </div>
+            </div>
+            <div>
+                {"'" + orgsTitle + "','" + orgsContent + "','" + orgsCategory + "'"}
+            </div>
+        </>
+    );
+}
+
+export {
+    OrgsAdd as default,
+};
