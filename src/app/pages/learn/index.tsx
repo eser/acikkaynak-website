4f3bae55dcbe5b45b2cdc8229380baa9f64f8942
Eser Ozvataf <eser@ozvataf.com>
2019-09-02 16:44:41 +0300

    Minor useEffect implementation for learn component.

--- src/app/pages/learn/index.tsx
+++ src/app/pages/learn/index.tsx
@@ -9,13 +9,22 @@ interface LearnProps {
 }
 
 function Learn(props: LearnProps) {
+    const [ content, setContent ] = useState(null);
+
+    useEffect(
+        () => {
+            setContent(`Talep edilen içerik yolu: /${props.contentPath}`);
+        },
+        [ props.contentPath ]
+    );
+
     return (
         <>
             <h1 className={bulmaStyles.title}>Eğitim</h1>
             <p>
                 Bu sayfa yapım aşamasında.<br />
                 <br />
-                Talep edilen içerik yolu: /{props.contentPath}
+                {content}
             </p>
         </>
     );
