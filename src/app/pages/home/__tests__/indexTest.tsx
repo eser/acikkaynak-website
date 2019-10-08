6003f288ac175707528bdae4679d50624a97a215
Eser Ozvataf <eser@ozvataf.com>
2019-10-09 00:03:51 +0300

    Fixed enzyme rendering issues on unit tests.

--- src/app/pages/home/__tests__/indexTest.tsx
+++ src/app/pages/home/__tests__/indexTest.tsx
@@ -1,9 +1,12 @@
+/**
+ * @jest-environment jsdom
+ */
 import React from 'react';
 
 import * as Enzyme from 'enzyme';
 import ReactSixteenAdapter from 'enzyme-adapter-react-16';
 
-import Home from '../home';
+import Home from '../index';
 
 Enzyme.configure({ adapter: new ReactSixteenAdapter() });
 
@@ -13,7 +16,12 @@ describe('Layout', () => {
     });
 
     it('should render', () => {
-        const wrapper = Enzyme.shallow(<Home />);
-        expect(wrapper.find('h1').contains('Home')).toEqual(true);
+        Enzyme.mount(<Home />);
+    });
+
+    it('should be properly rendered', () => {
+        const wrapper = Enzyme.mount(<Home />);
+
+        expect(wrapper.find('h1').contains('Darty React App')).toEqual(true);
     });
 });
