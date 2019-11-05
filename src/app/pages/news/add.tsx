db5c85fcec2c22be2f0f6b86621cea18085718dc
Samet Aylak <sametaylak29@gmail.com>
2019-11-05 20:02:20 +0300

    semantic-ui implemented for add news

--- src/app/pages/news/add.tsx
+++ src/app/pages/news/add.tsx
@@ -1,68 +1,49 @@
 import React, { useState } from 'react';
-import { useHistory, Link } from 'react-router-dom';
 
-import customAddNewsStyle from './add.less';
+import { Container, Header, Form, Button } from 'semantic-ui-react';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCaretSquareRight } from '@fortawesome/free-solid-svg-icons';
 
 function NewsAdd() {
-    const historyObj = useHistory();
+    const [newsTitle, setNewsTitle] = useState('');
+    const [newsContent, setNewsContent] = useState('');
+    const [newsTags, setNewsTags] = useState('');
 
-    const [ newsTitle, setNewsTitle ] = useState('');
-    const [ newsContent, setNewsContent ] = useState('');
-    const [ newsTags, setNewsTags ] = useState('');
-
-    function addNews() {
+    function onSubmitHandler(e) {
+        e.preventDefault();
         // TODO Add new development
     }
 
-    function goBackButton(event) {
-        historyObj.back();
-        event.preventDefault();
-    }
-
     return (
-        <>
-            <div className={customAddNewsStyle.newsContainer}>
-                <Link to="/news/" onClick={goBackButton}>
-                    Geri Dön
-                </Link>
-                <div className={customAddNewsStyle.form}>
-                    <h3>Haber Ekle</h3>
-                    <div className={customAddNewsStyle.field}>
-                        <p>Haber Başlığı</p>
-                        <textarea
-                            onChange={e => setNewsTitle(e.target.value)}
-                            className={customAddNewsStyle.newsTitle}
-                            cols={40}
-                            rows={3}
-                        />
-                    </div>
-                    <div className={customAddNewsStyle.field}>
-                        <p>Haber İçeriği</p>
-                        <textarea
-                            onChange={e => setNewsContent(e.target.value)}
-                            className={customAddNewsStyle.newsContent}
-                            cols={40}
-                            rows={10}
-                        />
-                    </div>
-                    <div className={customAddNewsStyle.field}>
-                        <p>Haber Etiketleri</p>
-                        <textarea
-                            className={customAddNewsStyle.tags}
-                            cols={40}
-                            rows={5}
-                            onChange={e => setNewsTags(e.target.value)}
-                        />
-                    </div>
-                </div>
-                <div className={customAddNewsStyle.button}>
-                    <button type="submit" onClick={addNews}>Ekle</button>
-                </div>
-                <div>
-                    {`'${newsTitle}','${newsContent}','${newsTags}'`}
-                </div>
-            </div>
-        </>
+        <Container className="content" textAlign="justified">
+            <Header as="h1">
+                <i aria-hidden="true" className="circular icon">
+                    <FontAwesomeIcon icon={faCaretSquareRight} />
+                </i>
+
+                <Header.Content>
+                    Haber Ekle
+                    <Header.Subheader>Açık Kaynak ile İlgili Haberler</Header.Subheader>
+                </Header.Content>
+            </Header>
+            
+            <Form onSubmit={onSubmitHandler}>
+                <Form.Field>
+                    <label>Haber Başlığı</label>
+                    <input value={newsTitle} onChange={e => setNewsTitle(e.target.value)} />
+                </Form.Field>
+                <Form.Field>
+                    <label>Haber İçeriği</label>
+                    <textarea value={newsContent} onChange={e => setNewsContent(e.target.value)} />
+                </Form.Field>
+                <Form.Field>
+                    <label>Haber Etiketleri</label>
+                    <input value={newsTags} onChange={e => setNewsTags(e.target.value)} />
+                </Form.Field>
+                <Button content="Ekle" type="submit" primary />
+            </Form>
+        </Container>
     );
 }
 
