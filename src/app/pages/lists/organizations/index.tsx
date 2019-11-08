2eb3950314c10bc640fc88b6e98a8a540c5ec404
Numan Can İpekçi <n.canipekci@gmail.com>
2019-11-08 23:25:46 +0300

    Refactored list pages.

--- src/app/pages/lists/organizations/index.tsx
+++ src/app/pages/lists/organizations/index.tsx
@@ -1,9 +1,15 @@
 import React, { useState, useEffect } from 'react';
 
-import { Container, Loader } from 'semantic-ui-react';
+import { Container, Loader, Header } from 'semantic-ui-react';
 
-import ListView from './listView';
+import { faUsers } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import ListView from '../listView';
+import listItemView from './listItemView';
 // import DetailView from './detailView';
+
+import localStyles from './style.less';
 
 const dataSourceUrl = 'https://api.acikkaynak.info/lists/organizations';
 
@@ -52,7 +58,19 @@ function Organizations() {
 
     return (
         <Container className="content">
-            <ListView datasource={organizations} />
+            <Header as="h1" className={localStyles.orgHeader}>
+                <i aria-hidden="true" className="circular icon">
+                    <FontAwesomeIcon icon={faUsers} />
+                </i>
+                <Header.Content>
+                    Organizasyonlar
+                    <Header.Subheader>
+                        Organizasyonlar Listesi
+                    </Header.Subheader>
+                </Header.Content>
+            </Header>
+
+            <ListView datasource={organizations} listItemView={listItemView} />
         </Container>
     );
 }
---
+++ src/app/pages/lists/projects/listItemView.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import ReactMarkdown from 'react-markdown';
+
+import { Item, Button, Label } from 'semantic-ui-react';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCodeBranch } from '@fortawesome/free-solid-svg-icons';
+
+
+function ListItem(props) {
+    return (
+        <Item className="project">
+            <Item.Content>
+                <Item.Header as="a" href={props.data.url}>
+                    {props.data.name}
+                </Item.Header>
+                <Item.Meta>
+                    <img src={`https://img.shields.io/github/stars/${props.data.githubUrl}.svg?style=social&amp;label=Star`} alt={`${props.data.name} stars`} />
+                </Item.Meta>
+                <Item.Description>
+                    <ReactMarkdown source={props.data.content} />
+                </Item.Description>
+                <Item.Extra>
+                    <Button as="a" primary floated="right" href={`https://github.com/${props.data.githubUrl}`}>
+                        {`github/${props.data.githubUrl}`}
+                    </Button>
+                    {props.data.needsContribution && (
+                        <Label>
+                            <FontAwesomeIcon icon={faCodeBranch} />
+                            Katılım Bekliyor
+                        </Label>
+                    )}
+                </Item.Extra>
+            </Item.Content>
+        </Item>
+    );
+}
+
+export default ListItem;
