162eb3cfde7f582ce043a61093810ca5c9f6cc88
SADIK KUZU <sadikkuzu@hotmail.com>
2021-05-11 19:27:49 +0300

    Typo fix on Support Us page

--- src/app/pages/support-us/index.tsx
+++ src/app/pages/support-us/index.tsx
@@ -16,7 +16,7 @@ function SupportUs() {
             <p>
                 Dijital mecralarda (GitHub, Twitter, YouTube, v.s.) bizi takip ediyor olmanız
                 ve iletişim kanallarımıza (Telegram, v.s.) dahil olmanız yeterli.
-                Düzenleyeyeceğimiz online/fiziksel tüm etkinliklere katılımınız ve çevrenize
+                Düzenleyeceğimiz online/fiziksel tüm etkinliklere katılımınız ve çevrenize
                 bizden bahsediyor olmanız bizlere en büyük desteği sağlayacaktır.
             </p>
         </Container>
