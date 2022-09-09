b7f96c08334fc79d87fb4279b4cba745ee815a41
Sinan Bekar <sinanbekar.work@gmail.com>
2022-09-10 00:33:53 +0300

    refactor: use button instead of next/link in theme-switcher

--- src/shared/layout/theme-switcher.tsx
+++ src/shared/layout/theme-switcher.tsx
@@ -1,7 +1,6 @@
 import { useTheme } from "next-themes";
 import { MoonIcon, SunIcon } from "@heroicons/react/24/solid";
 import { Conditional } from "@webclient/shared/react/conditional";
-import Link from "next/link";
 import styles from "./theme-switcher.module.css";
 
 interface ThemeSwitcherProps {
@@ -18,25 +17,19 @@ const ThemeSwitcher = (_props: ThemeSwitcherProps) => {
   };
 
   return (
-    <div>
-      <Conditional
-        if={theme !== "dark"}
-        then={
-          <Link href="/">
-            <a onClick={switchToTheme("dark")}>
-              <MoonIcon className={styles.icon} />
-            </a>
-          </Link>
-        }
-        otherwise={
-          <Link href="/">
-            <a onClick={switchToTheme("light")}>
-              <SunIcon className={styles.icon} />
-            </a>
-          </Link>
-        }
-      />
-    </div>
+    <Conditional
+      if={theme !== "dark"}
+      then={
+        <button onClick={switchToTheme("dark")}>
+          <MoonIcon className={styles.icon} />
+        </button>
+      }
+      otherwise={
+        <button onClick={switchToTheme("light")}>
+          <SunIcon className={styles.icon} />
+        </button>
+      }
+    />
   );
 };
 
