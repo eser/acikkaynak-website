5351ea7baed268195f1f7a33d0474eba63a5c447
Sinan Bekar <sinanbekar.work@gmail.com>
2022-10-07 23:16:33 +0300

    chore(deps): add @tailwindcss/line-clamp

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -60,5 +60,8 @@ module.exports = {
       },
     },
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/line-clamp"),
+  ],
 };
